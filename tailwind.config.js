a0a71f283f1c6e636685f51da500fa35104cb101
umairkhanzz <ibneyadam432062@gmail.com>
2024-03-21 22:08:27 +0500

    location page completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+    xs: "535px",
+
+    sm: "640px",
+
+    md: "768px",
+
+    lg: "1024px",
+
+    xl: "1280px",
+
+    "2xl": "1536px",
+  },
     extend: {},
   },
   plugins: [],
